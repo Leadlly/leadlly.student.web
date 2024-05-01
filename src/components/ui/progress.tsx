6f3c8b2642ce8872a9ec78c157fada800c49038e
adil-khursheed <adilkhursheed60@gmail.com>
2024-05-01 23:29:38 +0530

    fix:progress bar types error

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,9 +5,14 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface ICustomProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ICustomProgressProps
 >(({ className, value, indicatorClassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
