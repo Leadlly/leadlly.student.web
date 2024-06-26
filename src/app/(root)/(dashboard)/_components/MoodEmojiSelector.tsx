b49e47220a37398a70c14d1552055b4cf94104b4
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-06-26 17:57:48 +0530

    FEAT:Implement Mood Change

--- src/app/(root)/(dashboard)/_components/MoodEmojiSelector.tsx
+++ src/app/(root)/(dashboard)/_components/MoodEmojiSelector.tsx
@@ -1,40 +1,51 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import Image from "next/image";
+import React, { useState } from 'react';
+import Image from 'next/image';
 
-import { TMoodEmojisProps } from "@/helpers/types";
-import { cn } from "@/lib/utils";
+import { TMoodEmojisProps } from '@/helpers/types';
+import { cn } from '@/lib/utils';
+import apiClient from '@/apiClient/apiClient';
+import { toast } from 'sonner';
 
-const MoodEmojiSelector = ({
-  moodEmojis,
-}: {
-  moodEmojis: TMoodEmojisProps[];
-}) => {
-  const [currentMood, setCurrentMood] = useState("neutral-emoji");
+const MoodEmojiSelector = ({ moodEmojis }: { moodEmojis: TMoodEmojisProps[] }) => {
+	const [currentMood, setCurrentMood] = useState('neutral');
+	const changeMood = async (mood: string) => {
+		try {
+      console.log('ji')
+			const response = await apiClient.post('/api/user/todaysVibe/save', { todaysVibe: mood });
 
-  return (
-    <div className="relative flex items-center justify-between mt-2 max-w-48 w-full mx-auto">
-      <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
-      {moodEmojis.map((emoji) => (
-        <Image
-          key={emoji.mood_id}
-          src={emoji.mood}
-          alt={emoji.mood_id}
-          width={20}
-          height={20}
-          onClick={() => setCurrentMood(emoji.mood_id)}
-          className={cn(
-            "cursor-pointer transition-all ease-in duration-200",
-            currentMood === emoji.mood_id ? "grayscale-0" : "grayscale"
-          )}
-        />
-      ))}
-      <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
+			toast.success(response.data.message);
+			setCurrentMood(mood);
+		} catch (error: any) {
+			console.log(error, 'hello');
+			toast.error('Mood Change Failed', {
+				description: error.message,
+			});
+		}
+	};
+	return (
+		<div className='relative flex items-center justify-between mt-2 max-w-48 w-full mx-auto'>
+			<span className='w-[6px] h-[6px] rounded-full bg-primary'></span>
+			{moodEmojis.map((emoji) => (
+				<Image
+					key={emoji.mood_id}
+					src={emoji.moodImg}
+					alt={emoji.mood}
+					width={20}
+					height={20}
+					onClick={() => changeMood(emoji.mood)}
+					className={cn(
+						'cursor-pointer transition-all ease-in duration-200',
+						currentMood === emoji.mood ? 'grayscale-0' : 'grayscale'
+					)}
+				/>
+			))}
+			<span className='w-[6px] h-[6px] rounded-full bg-primary'></span>
 
-      <div className="absolute top-1/2 left-0 -translate-y-1/2 -z-10 bg-primary w-full h-[2px] bg-[#D9D9D9]"></div>
-    </div>
-  );
+			<div className='absolute top-1/2 left-0 -translate-y-1/2 -z-10 bg-primary w-full h-[2px] bg-[#D9D9D9]'></div>
+		</div>
+	);
 };
 
 export default MoodEmojiSelector;
