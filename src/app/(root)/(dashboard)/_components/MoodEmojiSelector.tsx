374411849bcea356871e0f596b59fb8f2dec642e
adil-khursheed <adilkhursheed60@gmail.com>
2024-07-11 00:40:09 +0530

    fix: initial study data form and emoji selector.

--- src/app/(root)/(dashboard)/_components/MoodEmojiSelector.tsx
+++ src/app/(root)/(dashboard)/_components/MoodEmojiSelector.tsx
@@ -10,6 +10,7 @@ import { getUser, setTodaysVibe } from "@/actions/user_actions";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { userData } from "@/redux/slices/userSlice";
 import { Loader2 } from "lucide-react";
+import { isMoodButtonDisabled } from "@/helpers/utils";
 
 const MoodEmojiSelector = () => {
   const user = useAppSelector((state) => state.user.user);
@@ -30,7 +31,6 @@ const MoodEmojiSelector = () => {
   );
 
   const [isCurrentMood, setIsCurrentMood] = useState(false);
-  const [todaysMoodSet, setIsTodaysMoodSet] = useState(false);
 
   const dispatch = useAppDispatch();
 
@@ -53,7 +53,7 @@ const MoodEmojiSelector = () => {
         })
       );
       toast.success(response.message);
-      setIsTodaysMoodSet(true);
+      // setIsTodaysMoodSet(true);
     } catch (error: any) {
       setCurrentMood(currentMood);
       toast.error("Saving your current mood failed!", {
@@ -64,56 +64,38 @@ const MoodEmojiSelector = () => {
     }
   };
 
-  useEffect(() => {
-    const now = new Date();
-    const nextMidnight = new Date();
-    nextMidnight.setHours(24, 0, 0, 0);
-
-    setIsTodaysMoodSet(now < nextMidnight);
-  }, []);
+  const isDisabled = isMoodButtonDisabled(
+    userCurrentMood?.[currentDateMoodIndex!]?.day!
+  );
 
   return (
-    <>
-      {todaysMoodSet ? (
-        <>
-          <p className="text-xs w-full text-center py-1 text-primary font-medium">
-            Your current mood is{" "}
-            {user?.details?.mood &&
-            user.details.mood.length &&
-            user?.details?.mood?.[currentDateMoodIndex!]?.emoji
-              ? user?.details?.mood?.[currentDateMoodIndex!]?.emoji
-              : "neutral"}
-          </p>
-        </>
-      ) : (
-        <div className="flex items-center">
-          <div className="relative flex items-center justify-between mt-2 max-w-48 w-full mx-auto">
-            <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
-            {moodEmojis.map((emoji) => (
-              <Image
-                key={emoji.mood_id}
-                src={emoji.moodImg}
-                alt={emoji.mood}
-                width={20}
-                height={20}
-                onClick={() => handleChangeMood(emoji.mood)}
-                className={cn(
-                  "cursor-pointer transition-all ease-in duration-200",
-                  currentMood === emoji.mood ? "grayscale-0" : "grayscale",
-                  isCurrentMood && "pointer-events-none opacity-90"
-                )}
-              />
-            ))}
-            <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
+    <div className="flex items-center">
+      <div className="relative flex items-center justify-between mt-2 max-w-48 w-full mx-auto">
+        <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
+        {moodEmojis.map((emoji) => (
+          <Image
+            key={emoji.mood_id}
+            src={emoji.moodImg}
+            alt={emoji.mood}
+            width={20}
+            height={20}
+            onClick={() => handleChangeMood(emoji.mood)}
+            className={cn(
+              "cursor-pointer transition-all ease-in duration-200",
+              currentMood === emoji.mood ? "grayscale-0" : "grayscale",
+              isCurrentMood && "pointer-events-none opacity-90",
+              isDisabled && "pointer-events-none opacity-90"
+            )}
+          />
+        ))}
+        <span className="w-[6px] h-[6px] rounded-full bg-primary"></span>
 
-            <div className="absolute top-1/2 left-0 -translate-y-1/2 -z-10 bg-primary w-full h-[2px] bg-[#D9D9D9]"></div>
-          </div>
-          {isCurrentMood && (
-            <Loader2 className="w-3 h-3 animate-spin mt-2 -ml-1 text-primary" />
-          )}
-        </div>
+        <div className="absolute top-1/2 left-0 -translate-y-1/2 -z-10 bg-primary w-full h-[2px] bg-[#D9D9D9]"></div>
+      </div>
+      {isCurrentMood && (
+        <Loader2 className="w-3 h-3 animate-spin mt-2 -ml-1 text-primary" />
       )}
-    </>
+    </div>
   );
 };
 
