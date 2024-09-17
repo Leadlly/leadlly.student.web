6f95df04e35b0e3771b1f8c91387ff423bafee8a
Danish <dan71ish@gmail.com>
2024-09-17 09:39:13 +0530

    question analysis from response

--- src/app/(quiz)/quiz/[quizId]/report/components/Defaultview.tsx
+++ src/app/(quiz)/quiz/[quizId]/report/components/Defaultview.tsx
@@ -1,32 +1,64 @@
+"use client";
 import TopicsCovered from "./TopicCovered";
 import Score from "./Score";
 import AttemptAnalysis from "./AttemptAnalysis";
 import TopicsEfficiency from "./TopicEfficiency";
 import SolutionAnalysis from "./SolutionAnalysis";
 import QuizDetails from "./QuizDetails";
-import BackButton from "./BackButton";
+import { getQuizReport } from "@/actions/weekly_quiz_actions";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 type Props = {};
+
+interface topicWithEfficiency {
+  topic: string;
+  efficiency: number;
+}
+export type ReportSchema = {
+  questions: string[];
+  status: number;
+  topicsWithEfficiency: topicWithEfficiency[];
+};
+
 const Defaultview = (props: Props) => {
+  const { quizId } = useParams();
+
+  const [report, setReport] = useState<ReportSchema | null>(null);
+
+  const fetchQuizReport = useCallback(async () => {
+    try {
+      const report = await getQuizReport(quizId as string);
+      setReport(report);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [quizId]);
+
+  useEffect(() => {
+    if (quizId) {
+      fetchQuizReport();
+    }
+  }, [quizId, fetchQuizReport]);
+
   return (
     <div>
-      {" "}
       <header>
-        {" "}
         <div className="flex justify-center w-full p-4">
           <h1 className=" text-2xl md:text-4xl font-semibold">Quiz Report</h1>
         </div>
         <div className="flex flex-col  items-start justify-between p-5 pb-6 border-b bg-[#9654F42E] gap-16 rounded-lg m-5">
-          {" "}
           <QuizDetails />
         </div>
-      </header>{" "}
-      <TopicsCovered />
+        <div className="flex">
+          <TopicsCovered />
+          <TopicsEfficiency report={report} />
+        </div>
+      </header>
       <div className="flex flex-1 gap-5 max-md:mx-5">
         <AttemptAnalysis />
         <Score />
       </div>
-      <TopicsEfficiency />
-      <SolutionAnalysis />
+      {report && <SolutionAnalysis question={report.questions} />}
     </div>
   );
 };
