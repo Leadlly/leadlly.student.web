6d85df2aa46111a9cc294abf718cd79185395d40
shivang-16 <shivangyadav121@gmail.com>
2024-09-27 16:21:15 +0530

    refactor: routing

--- src/app/(subscription)/trial-subscription/_components/ActivateFreeTrialButton.tsx
+++ src/app/(subscription)/trial-subscription/_components/ActivateFreeTrialButton.tsx
@@ -2,7 +2,7 @@
 
 import { getFreeTrialActive } from "@/actions/subscription_actions";
 import { Button } from "@/components/ui/button";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks"; // Import useAppSelector to access Redux state
 import { userData } from "@/redux/slices/userSlice";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -13,8 +13,11 @@ const ActivateFreeTrialButton = () => {
   const [isActivating, setIsActivating] = useState(false);
 
   const dispatch = useAppDispatch();
-
   const router = useRouter();
+
+  // Get user data from Redux state
+  const user = useAppSelector((state) => state.user.user);
+  const isFreeTrialAvailed = user?.freeTrial?.availed; // Check if the free trial has been availed
 
   const onClickHandler = async () => {
     setIsActivating(true);
@@ -32,19 +35,22 @@ const ActivateFreeTrialButton = () => {
       setIsActivating(false);
     }
   };
+
   return (
     <Button
       onClick={onClickHandler}
       size="lg"
       className="text-base md:text-lg max-w-64 w-full"
-      disabled={isActivating}
+      disabled={isActivating || isFreeTrialAvailed} // Disable button if activating or free trial is availed
     >
       {isActivating ? (
         <span className="flex items-center">
           <Loader2 className="w-5 h-5 animate-spin" />
         </span>
+      ) : isFreeTrialAvailed ? (
+        "Free Trial Already Availed" 
       ) : (
-        "Start the 21-Day Challenge"
+        "Start Free Trial"
       )}
     </Button>
   );
