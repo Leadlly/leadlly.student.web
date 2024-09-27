5f01ddf2478071beba5512a3b119349a059316c7
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-09-27 23:22:08 +0530

    update gender, standard and exam

--- src/app/(student-initial-info)/initial-info/_components/InitialInfoForm.tsx
+++ src/app/(student-initial-info)/initial-info/_components/InitialInfoForm.tsx
@@ -168,31 +168,31 @@ export default function StudentInitialInfoForm() {
 
   return (
     <>
-     <div className="flex items-center mb-2 mt-2"> 
-  <Button
-    variant="ghost"
-    size="icon"
-    onClick={handlePrevious}
-    className="mr-1"
-    disabled={step === 0}
-  >
-    <AiOutlineArrowLeft className="h-6 w-6" />
-  </Button>
+      <div className="flex items-center mb-2 mt-2">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={handlePrevious}
+          className="mr-1"
+          disabled={step === 0}
+        >
+          <AiOutlineArrowLeft className="h-6 w-6" />
+        </Button>
 
-  <div className="w-full flex justify-center items-center gap-2">
-    <div className="flex items-center rounded-[5px] gap-1 sm:gap-[8px] w-[85%] sm:w-[80%] mx-auto">
-      {[...Array(5)].map((_, index) => (
-        <div key={index} className="flex-1 flex items-center">
-          <div
-            className={`h-[4px] sm:h-[6px] md:h-[8px] ${index < step ? 'bg-purple-500' : 'bg-gray-200'} rounded-[3px] transition-colors duration-300`}
-            style={{ width: '100%' }}
-          ></div>
-          {index < 5 - 1 && <div className="w-[2px] sm:w-[3px] md:w-[4px]"></div>} 
+        <div className="w-full flex justify-center items-center gap-2">
+          <div className="flex items-center rounded-[5px] gap-1 sm:gap-[8px] w-[85%] sm:w-[80%] mx-auto">
+            {[...Array(5)].map((_, index) => (
+              <div key={index} className="flex-1 flex items-center">
+                <div
+                  className={`h-[4px] sm:h-[6px] md:h-[8px] ${index < step ? 'bg-purple-500' : 'bg-gray-200'} rounded-[3px] transition-colors duration-300`}
+                  style={{ width: '100%' }}
+                ></div>
+                {index < 5 - 1 && <div className="w-[2px] sm:w-[3px] md:w-[4px]"></div>}
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
-  </div>
-</div>
+      </div>
 
       <div className="flex relative top-[2%] md:top-[10%] justify-between px-4 pb-4">
         <div className="bg-white rounded-lg w-full">
@@ -258,128 +258,126 @@ export default function StudentInitialInfoForm() {
 
 
 
-{step === 2 && (
-  <div className="space-y-4 flex flex-col items-center justify-center">
-    <h2 className="text-3xl lg:text-4xl font-bold mb-2">What is your Gender?</h2>
-    <p className="text-gray-600 text-center mb-4 text-lg lg:text-xl"> 
-      Select your gender from the given below options
-    </p>
-    <RadioGroup
-      onValueChange={(value) => handleSelectChange('gender', value)}
-      value={formData.gender}
-      className="flex flex-col items-center space-y-4"
-    >
-      <div className="flex gap-[50px] space-x-4">
-        {['Female', 'Male'].map((gender) => (
-          <button
-            onClick={() => handleSelectChange('gender', gender)}
-            key={gender}
-            className="flex flex-col items-center"
-          >
-            <RadioGroupItem value={gender} id={gender} className="sr-only" />
-            <Label
-              htmlFor={gender}
-              className={`cursor-pointer flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.gender === gender ? 'border-purple-500' : 'border-gray-200'
-                }`}
-              style={{ width: '160px', height: '160px' }} 
-            >
-              <Image
-                src={genderImages[gender as keyof GenderImages]}
-                alt={gender}
-                width={100}
-                height={100}
-                className="mb-2"
-              />
-              <span className="capitalize text-center text-lg lg:text-xl">{gender}</span> 
-            </Label>
-          </button>
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <button
-          onClick={() => handleSelectChange('gender', 'Other')}
-          className="flex flex-col items-center"
-        >
-          <RadioGroupItem value="Other" id="Other" className="sr-only" />
-          <Label
-            htmlFor="Other"
-            className={`cursor-pointer flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.gender === 'Other' ? 'border-purple-500' : 'border-gray-200'
-              }`}
-              style={{ width: '140px', height: '140px', minWidth: '140px', minHeight: '140px', maxWidth: '200px', maxHeight: '200px' }} 
-          >
-            <Image
-              src={genderImages['Other']}
-              alt="Other"
-              width={100} // Increased image width
-              height={100} // Increased image height
-              className="mb-1"
-            />
-            <span className="capitalize text-center text-lg lg:text-xl">Other</span> 
-          </Label>
-        </button>
-      </div>
-    </RadioGroup>
-  </div>
-)}
+                {step === 2 && (
+                  <div className="space-y-4 flex flex-col items-center justify-center">
+                    <h2 className="text-3xl lg:text-4xl font-bold mb-2">What is your Gender?</h2>
+                    <p className="text-gray-600 text-center mb-4 text-lg lg:text-xl">
+                      Select your gender from the given below options
+                    </p>
+                    <RadioGroup
+                      onValueChange={(value) => handleSelectChange('gender', value)}
+                      value={formData.gender}
+                      className="flex flex-col items-center space-y-4"
+                    >
+                      <div className="flex gap-[50px] space-x-4">
+                        {['Female', 'Male'].map((gender) => (
+                          <button
+                            onClick={() => handleSelectChange('gender', gender)}
+                            key={gender}
+                            className="flex flex-col items-center"
+                          >
+                            <RadioGroupItem value={gender} id={gender} className="sr-only" />
+                            <Label
+                              htmlFor={gender}
+                              className={`cursor-pointer flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.gender === gender ? 'border-purple-500' : 'border-none'} bg-gray-100`}
+                              style={{ width: '120px', height: '120px' }}
+                            >
+
+                              <Image
+                                src={genderImages[gender as keyof GenderImages]}
+                                alt={gender}
+                                width={100}
+                                height={100}
+                                className="mb-2"
+                              />
+                              <span className="capitalize text-center text-lg lg:text-xl">{gender}</span>
+                            </Label>
+                          </button>
+                        ))}
+                      </div>
+                      <div className="flex justify-center">
+                        <button
+                          onClick={() => handleSelectChange('gender', 'Other')}
+                          className="flex flex-col items-center"
+                        >
+                          <RadioGroupItem value="Other" id="Other" className="sr-only" />
+                          <Label
+                            htmlFor="Other"
+                            className={`cursor-pointer flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.gender === 'Other' ? 'border-purple-500' : 'border-none'} bg-gray-100`}
+                            style={{ width: '120px', height: '120px' }}
+                          >
+                            <Image
+                              src={genderImages['Other']}
+                              alt="Other"
+                              width={100} // Increased image width
+                              height={100} // Increased image height
+                              className="mb-1"
+                            />
+                            <span className="capitalize text-center text-lg lg:text-xl">Other</span>
+                          </Label>
+                        </button>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                )}
 
 
-{step === 3 && (
-  <div className="space-y-4 flex flex-col items-center justify-center">
-    <h2 className="text-2xl lg:text-4xl font-bold mb-2">Which class are you studying?</h2>
-    <p className="text-gray-600 text-center mb-4 text-base lg:text-xl"> 
-      Focus on core topics with hands-on practice and real-world examples for deeper understanding.
-    </p>
-    <div className="flex flex-col items-center">
-    <div className="flex gap-[50px] space-x-4 mb-4">
-  {['Eleven', 'Twelve'].map((classOption) => (
-    <button
-      key={classOption}
-      type="button"
-      onClick={() => {
-        handleSelectChange('class', classOption);
-        handleNext();
-      }}
-      className={`flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.class === classOption ? 'border-purple-500' : 'border-gray-200'}`}
-      style={{ width: '140px', height: '140px', minWidth: '140px', minHeight: '140px', maxWidth: '200px', maxHeight: '200px' }} 
-    >
-      <Image
-        src={classImage[classOption as keyof ClassImage]}
-        alt={classOption}
-        width={113} 
-        height={113} 
-        className="mb-2"
-      />
-      <span className="capitalize text-base lg:text-xl">{classOption}</span>
-    </button>
-  ))}
-</div>
+                {step === 3 && (
+                  <div className="space-y-4 flex flex-col items-center justify-center">
+                    <h2 className="text-2xl sm:text-3xl md:text-4xl text-center font-bold mb-4 whitespace-nowrap">Which class are you studying?</h2>
+                    <p className="text-gray-600 text-center mb-4 text-base lg:text-xl">
+                      Focus on core topics with hands-on practice and real-world examples for deeper understanding.
+                    </p>
+                    <div className="flex flex-col items-center">
+                      <div className="flex gap-[50px] space-x-4 mb-4">
+                        {['Eleven', 'Twelve'].map((classOption) => (
+                          <button
+                            key={classOption}
+                            type="button"
+                            onClick={() => {
+                              handleSelectChange('class', classOption);
+                              handleNext();
+                            }}
+                            className={`flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.class === classOption ? 'border-purple-500' : 'border-none'} bg-gray-100`}
+                            style={{ width: '120px', height: '120px' }}
+                          >
+                            <Image
+                              src={classImage[classOption as keyof ClassImage]}
+                              alt={classOption}
+                              width={113}
+                              height={113}
+                              className="mb-2"
+                            />
+                            <span className="capitalize text-base lg:text-xl">{classOption}</span>
+                          </button>
+                        ))}
+                      </div>
 
 
-      <div className="flex justify-center">
-        <button
-          key="Dropper"
-          type="button"
-          onClick={() => {
-            handleSelectChange('class', 'Dropper');
-            handleNext();
-          }}
-          className={`flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.class === 'Dropper' ? 'border-purple-500' : 'border-gray-200'
-            }`}
-          style={{ width: '140px', height: '140px' }} 
-        >
-          <Image
-            src={classImage['Dropper']}
-            alt="Dropper"
-            width={64}
-            height={64}
-            className="mb-2"
-          />
-          <span className="capitalize text-base lg:text-xl">Dropper</span>
-        </button>
-      </div>
-    </div>
-  </div>
-)}
+                      <div className="flex justify-center">
+                        <button
+                          key="Dropper"
+                          type="button"
+                          onClick={() => {
+                            handleSelectChange('class', 'Dropper');
+                            handleNext();
+                          }}
+                          className={`flex flex-col justify-center items-center p-4 border-2 rounded-lg ${formData.class === 'Dropper' ? 'border-purple-500' : 'border-none'} bg-gray-100`}
+                          style={{ width: '140px', height: '140px' }}
+                        >
+                          <Image
+                            src={classImage['Dropper']}
+                            alt="Dropper"
+                            width={64}
+                            height={64}
+                            className="mb-2"
+                          />
+                          <span className="capitalize text-base lg:text-xl">Dropper</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
 
 
 
@@ -387,56 +385,56 @@ export default function StudentInitialInfoForm() {
                   <div className="space-y-4 flex flex-col items-center justify-center">
                     <h2 className="text-2xl font-bold mb-2">Are your Studying for?</h2>
                     <p className="text-gray-600 text-center mb-4 text-sm sm:text-base md:text-lg">
-  Focus on core topics with hands-on practice and real-world examples for deeper understanding.
-</p>
+                      Focus on core topics with hands-on practice and real-world examples for deeper understanding.
+                    </p>
 
-                    <div className="flex justify-center gap-4 sm:gap-8 md:gap-10 lg:gap-24"> 
-  {['jee', 'neet'].map((exam) => (
-    <button
-      key={exam}
-      type="button"
-      onClick={() => {
-        handleSelectChange('competitiveExam', exam);
-        handleNext();
-      }}
-      className={`relative flex flex-col justify-center items-center p-4 border-2 rounded-lg shadow-md ${formData.competitiveExam === exam ? 'border-purple-500' : 'border-gray-200'}`}
-      style={{
-        width: '140px',
-        height: '180px',
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-      }}
-    >
-      <div
-        className="flex items-center justify-center"
-        style={{
-          width: '100%',
-          height: '70%',
-          color: exam === 'jee' ? '#2D61FD' : '#06E480',
-          fontSize: '24px',
-          fontWeight: 'bold',
-        }}
-      >
-        {exam.toUpperCase()}
-      </div>
+                    <div className="flex justify-center gap-4 sm:gap-8 md:gap-10 lg:gap-24">
+                      {['jee', 'neet'].map((exam) => (
+                        <button
+                          key={exam}
+                          type="button"
+                          onClick={() => {
+                            handleSelectChange('competitiveExam', exam);
+                            handleNext();
+                          }}
+                          className={`relative flex flex-col justify-center items-center p-4 border-2 rounded-lg shadow-md ${formData.competitiveExam === exam ? 'border-purple-500' : 'border-none'} bg-gray-100`}
+                          style={{
+                            width: '140px',
+                            height: '180px',
+                            boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+                          }}
+                        >
+                          <div
+                            className="flex items-center justify-center"
+                            style={{
+                              width: '100%',
+                              height: '70%',
+                              color: exam === 'jee' ? '#2D61FD' : '#06E480',
+                              fontSize: '24px',
+                              fontWeight: 'bold',
+                            }}
+                          >
+                            {exam.toUpperCase()}
+                          </div>
 
-      <Image
-        src={examImages[exam as keyof ExamImages].topRight}
-        alt={`${exam}-top-right`}
-        width={77}
-        height={77}
-        className="absolute top-[-0.5rem] right-[-0.5rem] overflow-hidden"
-      />
+                          <Image
+                            src={examImages[exam as keyof ExamImages].topRight}
+                            alt={`${exam}-top-right`}
+                            width={77}
+                            height={77}
+                            className="absolute top-[-0.5rem] right-[-0.5rem] overflow-hidden"
+                          />
 
-      <Image
-        src={examImages[exam as keyof ExamImages].bottomLeft}
-        alt={`${exam}-bottom-left`}
-        width={70}
-        height={50}
-        className="absolute bottom-2 left-[-0.5rem]"
-      />
-    </button>
-  ))}
-</div>
+                          <Image
+                            src={examImages[exam as keyof ExamImages].bottomLeft}
+                            alt={`${exam}-bottom-left`}
+                            width={70}
+                            height={50}
+                            className="absolute bottom-2 left-[-0.5rem]"
+                          />
+                        </button>
+                      ))}
+                    </div>
 
                   </div>
                 )}
