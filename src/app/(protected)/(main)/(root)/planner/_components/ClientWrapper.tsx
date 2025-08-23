8382a456b2b5fbefb326e895930692f61be2d016
adil-khursheed <adilkhursheed60@gmail.com>
2025-08-23 15:42:44 +0530

    fix: optimize planner

--- src/app/(protected)/(main)/(root)/planner/_components/ClientWrapper.tsx
+++ src/app/(protected)/(main)/(root)/planner/_components/ClientWrapper.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import React, { useState } from "react";
 import { PlannerDataProps, TDayProps } from "@/helpers/types";
 import DesktopUI from "./DesktopUI";
 import MobileUI from "./MobileUI";
 import TabletUI from "./TabletUI";
-import React, { useState } from "react";
 import { getFormattedDate } from "@/helpers/utils";
+import { useMediaQuery } from "usehooks-ts";
 
 const ClientWrapper = ({ data }: { data: PlannerDataProps }) => {
   const [todaysData, setTodaysData] = useState<TDayProps | null>(
@@ -18,26 +19,36 @@ const ClientWrapper = ({ data }: { data: PlannerDataProps }) => {
       )[0]) ||
       null
   );
-  return (
-    <>
-      <DesktopUI
-        plannerData={data}
-        todaysTopics={todaysData}
-        setData={setTodaysData}
-      />
 
-      <TabletUI
-        plannerData={data}
-        todaysTopics={todaysData}
-        setData={setTodaysData}
-      />
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isTablet = useMediaQuery("(max-width: 1280px)");
 
+  if (isMobile) {
+    return (
       <MobileUI
         plannerData={data}
         todaysTopics={todaysData}
         setData={setTodaysData}
       />
-    </>
+    );
+  }
+
+  if (isTablet) {
+    return (
+      <TabletUI
+        plannerData={data}
+        todaysTopics={todaysData}
+        setData={setTodaysData}
+      />
+    );
+  }
+
+  return (
+    <DesktopUI
+      plannerData={data}
+      todaysTopics={todaysData}
+      setData={setTodaysData}
+    />
   );
 };
 
